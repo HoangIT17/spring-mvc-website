bcb48febfa11162a17bab47c5a52186c4cf4738f
HoangIT17 <hpham172002@gmai.com>
2025-07-19 02:19:57 +0700

    updatr jsp

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,31 +1,41 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-    
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from Controller"; // Assuming you have a home.html template
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
     
+    //DI: Dependency Injection
     private UserService userService;
     
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
     @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello(); // Assuming you have a home.html template
+        String test = this.userService.handleHello();
+        return "hello"; // Assuming you have a home.html template
     }
 }
+
+// @RestController
+// public class UserController {
+    
+//     //DI: Dependency Injection
+//     private UserService userService;
+    
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello(); // Assuming you have a home.html template
+//     }
+// }
