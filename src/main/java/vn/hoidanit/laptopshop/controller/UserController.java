dab23460af840ebced9621388afb3cff3bb1d77d
HoangIT17 <hpham172002@gmai.com>
2025-07-19 18:02:03 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,9 +1,12 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -17,25 +20,24 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("hoangit",test);
+        model.addAttribute("hoidanit", "From Controller with Model");
+        return "hello"; // Assuming you have a home.html template
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        model.addAttribute("hoidanit", "From Controller with Model");
+        return "admin/user/create"; // Assuming you have a home.html template
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoangit) {
+        System.out.println("run here" + hoangit);
         return "hello"; // Assuming you have a home.html template
     }
 }
-
-// @RestController
-// public class UserController {
-    
-//     //DI: Dependency Injection
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return this.userService.handleHello(); // Assuming you have a home.html template
-//     }
-// }
